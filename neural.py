4c95e18e0d8f9c3fbf4e65f0ac65365fef37d580
itsMahdi <mahdipourghasem@gmail.com>
2018-09-01 17:08:19 +0430

    complete train_neural_network function

--- neural.py
+++ neural.py
@@ -13,7 +13,8 @@ batch_size = 100
 x = tf.placeholder('float',[None , 784])
 y = tf.placeholder('float')
 
-def neural_networl_model(data):
+
+def neural_network_model(data):
 
     hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784,n_nodes_hl1])),
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
@@ -34,10 +35,34 @@ def neural_networl_model(data):
     l2 = tf.nn.relu(l2)
 
     l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']),hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l1)
+    l3 = tf.nn.relu(l3)
 
     output = tf.add(tf.matmul(l3,output_layer['weights']),output_layer['biases'])
 
     return output
+
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    # optimizer = tf.train.AdamOptimizer(cost)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    hm_epochs = 10
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x , epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer , cost], feed_dict={x:epoch_x,y:epoch_y})
+                epoch_loss += c
+            print("EPOCH ", epoch,"completed out of",hm_epochs,'loss',epoch_loss)
+        correct = tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+        print('accuracy',accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
+
+train_neural_network(x)
+
 
 
